ebd49996db75ac09e65606a1bcb97a3dcbab28eb
caimingcheng <2225066948@qq.com>
2019-09-23 11:31:17 +0800

    新增咖啡菜单

--- src/main/java/design/patterns/iterator/simpleDemo/Waitress.java
+++ src/main/java/design/patterns/iterator/simpleDemo/Waitress.java
@@ -5,19 +5,24 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator<MenuItem> pancakeHouseMenuIterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> dinerMenuIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeMenuIterator = cafeMenu.createIterator();
         System.out.println("Menu\n------\nBREAKFAST");
         printMenu(pancakeHouseMenuIterator);
         System.out.println("\nLUNCH");
         printMenu(dinerMenuIterator);
+        System.out.println("\nDinner");
+        printMenu(cafeMenuIterator);
     }
 
     private void printMenu(Iterator<MenuItem> iterator) {
