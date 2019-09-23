b248f801c624081103a5e25b8daef512a0907be4
caimingcheng <2225066948@qq.com>
2019-09-23 15:12:39 +0800

    组合模式

---
+++ src/main/java/design/patterns/iterator/composite/Menu.java
@@ -0,0 +1,57 @@
+package design.patterns.iterator.composite;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * 菜单
+ */
+public class Menu extends MenuComponent{
+    private ArrayList<MenuComponent> menuComponents = new ArrayList();
+    private String name;
+    private String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    @Override
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    @Override
+    public MenuComponent getChild(int i) {
+        return menuComponents.get(i);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void print() {
+        System.out.print("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("------------------");
+
+        //菜单也打印所有的菜单项（此处使用迭代器遍历，并递归打印）
+        Iterator<MenuComponent> iterator = menuComponents.iterator();
+        while (iterator.hasNext()) {
+            MenuComponent menuComponent = iterator.next();
+            menuComponent.print();
+        }
+    }
+}
---
+++ src/test/java/design/patterns/iterator/simpleDemo/CompositeMenuTestDrive.java
@@ -0,0 +1,34 @@
+package design.patterns.iterator.simpleDemo;
+
+import design.patterns.iterator.composite.Menu;
+import design.patterns.iterator.composite.MenuComponent;
+import design.patterns.iterator.composite.MenuItem;
+import design.patterns.iterator.composite.Waitress;
+
+public class CompositeMenuTestDrive {
+    public static void main(String[] args) {
+        MenuComponent pancakeHouseMenu = new Menu("PANCAKE HOUSE MENU", "Breakfast");
+        MenuComponent dinerMenu = new Menu("DINER MENU", "Lunch");
+        MenuComponent cafeMenu = new Menu("CAFE MENU", "Dinner");
+        MenuComponent dessertMenu = new Menu("DESSERT MENU", "Dessert of course");
+        MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
+
+        allMenus.add(pancakeHouseMenu);
+        allMenus.add(dinerMenu);
+        allMenus.add(cafeMenu);
+
+        pancakeHouseMenu.add(new MenuItem("Regular Pancake Breakfast","Pancakes with fried eggs, sausage",false,2.99));
+        pancakeHouseMenu.add(new MenuItem("Blueberry Pancake Breakfast","Pancakes with fresh blueberries",true,3.49));
+
+        //dinerMenu中包含dessertMenu子菜单
+        dinerMenu.add(new MenuItem("Pasta","Spaghetti with Marinara Sauce, and a slice of sourdough bread",true,3.89));
+        dinerMenu.add(dessertMenu);
+        dessertMenu.add(new MenuItem("Apple pie","Apple pie with a flakey crust, topped with vanilla ice cream",true,1.59));
+
+        cafeMenu.add(new MenuItem("Veggie Burger and Air Fries","Veggie burger on a whole wheat bun, lettuce, tomato, and fries",true,3.99));
+        cafeMenu.add(new MenuItem("Soup of the day","A cup of the soup of the day, with a side salad",false,3.69));
+
+        Waitress waitress = new Waitress(allMenus);
+        waitress.printMenu();
+    }
+}
